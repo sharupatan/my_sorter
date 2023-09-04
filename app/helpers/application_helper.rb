7d3d5aee1700fa3b3e41b65efe79476d503b2c37
sharupatan <patansharu654321@gmail.com>
2023-09-05 00:04:56 +0530

    topics view section in home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def covered_topics
+		[
+			'decorators', 'resource routing',
+			'implemented locales', 'implemented status filter'
+		]
+	end
+
+	def display_topic(topic)
+		content_tag(:div,topic, class: 'alert alert-primary mb-3')
+	end
 end
